56b42084e84726acd4ddc8ac6d659038e375d990
Niels Disveld <nielsdisveld@gmail.com>
2025-08-23 23:33:54 +0200

    Some doc

--- src/IslandSolver.hs
+++ src/IslandSolver.hs
@@ -2,6 +2,7 @@ module IslandSolver (islands) where
 
 import Line (Line, zeroes)
 
+-- | Count how many islands on given input
 islands :: [Line] -> Int
 islands = fst . foldr go (0, zeroes)
   where
@@ -10,6 +11,7 @@ islands = fst . foldr go (0, zeroes)
           new = countNew zipped
        in (acc + new, ln)
 
+-- | Compare line to previous line and count how many new islands are starting
 countNew :: [(Int, Int)] -> Int
 countNew = go 0
   where
@@ -20,8 +22,10 @@ countNew = go 0
         (isNew, rest) = consumeOnes xs
         n = if isNew then 1 else 0
 
+-- | Consume one '1' or more from the line while also checking if it is starting
+-- a new island
 consumeOnes :: [(Int, Int)] -> (Bool, [(Int, Int)])
 consumeOnes [] = (True, [])
 consumeOnes ((0, 1) : xs) = consumeOnes xs
-consumeOnes ((_, 0) : xs) = (True, xs)
 consumeOnes ((1, 1) : xs) = (False, dropWhile ((==) 1 . snd) xs)
+consumeOnes ((_, 0) : xs) = (True, xs)
