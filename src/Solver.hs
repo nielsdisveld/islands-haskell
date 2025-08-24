6e83837e120be39428ed4ddfc931df83971c815f
Niels Disveld <nielsdisveld@gmail.com>
2025-08-24 09:26:12 +0200

    Some doc

--- src/Solver.hs
+++ src/Solver.hs
@@ -11,9 +11,9 @@ type TwoLines = [(Bool, Bool)]
 solve :: [Line] -> Int
 solve = fst . foldr go (0, zeroes)
   where
-    go ln (acc, ln0) =
-      let zipped = zip ln0 ln
-          new = countNew zipped
+    go ln (acc, prev) =
+      let twoLines = zip prev ln
+          new = countNew twoLines
        in (acc + new, ln)
 
 -- | Compare line to previous line and count how many new islands are starting.
@@ -26,15 +26,15 @@ countNew = go 0
         let (n, rest) = consumeOnes xs
          in go (acc + n) rest
 
--- | Consume contiguoous sequence of '0's.
+-- | Consume contiguous sequence of '0's.
 consumeZeroes :: TwoLines -> TwoLines
 consumeZeroes = dropWhile ((==) False . snd)
 
--- | Consumes a contiguous sequence of '1's from the beginning of the line.
+-- | Consumes a contiguous sequence of '1's.
 --
 -- Returns a tuple:
 --   (1, rest) if a new island was found
---   (0, rest) if the found island was not new (connected to previous line)
+--   (0, rest) if the found island was not new (connected to an island on previous line)
 --
 -- 'rest' is the remaining portion of the line after consuming the contiguous '1's.
 --
@@ -48,6 +48,6 @@ consumeZeroes = dropWhile ((==) False . snd)
 consumeOnes :: TwoLines -> (Int, TwoLines)
 consumeOnes [] = (1, [])
 consumeOnes (p : ps) = case p of
-  (False, True) -> consumeOnes ps -- Entry above was not an island
-  (True, True) -> (0, dropWhile ((==) True . snd) ps) -- Entry above is an island so current is not a new one
-  (_, False) -> (1, ps) -- End of island which was new
+  (False, True) -> consumeOnes ps -- Entry above was not an island.
+  (True, True) -> (0, dropWhile ((==) True . snd) ps) -- Entry above is an island so current is not a new one.
+  (_, False) -> (1, ps) -- End of island which was new.
