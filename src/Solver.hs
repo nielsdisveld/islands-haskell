9ebda1a96df958dd841ef158014d748375f66c16
Niels Disveld <nielsdisveld@gmail.com>
2025-09-23 23:36:10 +0200

    Documentation

--- src/Solver.hs
+++ src/Solver.hs
@@ -12,13 +12,14 @@ type Marker = Int
 -- | A tuple that represents the current point (Bool) compared to the marked previous point
 type ComparePoints = (Int, Bool)
 
--- | List of ComparePoint
+-- | List of ComparePoint which can be seen as a 2-wide grid
 type CompareLines = Line ComparePoints
 
 data IslandState = None | New | Connected Int
   deriving (Eq, Show)
 
 -- | Count how many islands on given input.
+-- Assumed is that all line are of equal length
 solve :: [Line Bool] -> Count
 solve = fstOf3 . foldr go (0, 1, zeroes)
   where
@@ -27,8 +28,8 @@ solve = fstOf3 . foldr go (0, 1, zeroes)
       let twoLines = zip prev ln
        in countNew count marker twoLines
 
--- | Compare line to previous line and count how many new islands are starting. Also return a
--- transformed version of current line where the islands are labeled
+-- | Compare line to previous line and count how many new islands are starting or merging.
+-- Also return a transformed version of current line where the islands are labeled
 countNew :: Count -> Marker -> CompareLines -> (Count, Marker, Line Int)
 countNew count marker lns =
   case splitConnected lns of
@@ -41,7 +42,7 @@ countNew count marker lns =
           (finalCount, finalMarker, finalTransformed) = countNew newCount newMarker rest
        in (finalCount, finalMarker, transformed ++ finalTransformed)
 
--- | Determine the state of a connected compare line
+-- | Determine the state of a connected 2-wide grid
 exploreConnected :: Count -> IslandState -> CompareLines -> (Count, IslandState)
 exploreConnected count state (p1 : p2 : rest) =
   case (state, next state p1 p2) of
@@ -50,7 +51,7 @@ exploreConnected count state (p1 : p2 : rest) =
 exploreConnected count New _ = (count + 1, New) -- Found a new island (not connected to previous line)
 exploreConnected count state _ = (count, state)
 
--- | Break two compare lines so that we split of 1 connected compare line to analyze
+-- | Break two compare lines so that we split of a connected 2-wide grid from the rest
 splitConnected :: CompareLines -> (CompareLines, CompareLines)
 splitConnected [] = ([], [])
 splitConnected [x] = ([x], [])
@@ -61,7 +62,7 @@ splitConnected (x : y : xs) =
        in (x : land, rest)
     else ([x], y : xs)
 
--- | Determines if 2 compare points are connected
+-- | Determines if a 2x2 grid is connected
 isConnected :: ComparePoints -> ComparePoints -> Bool
 isConnected p1 p2 =
   case (p1, p2) of
@@ -73,13 +74,13 @@ isConnected p1 p2 =
     ((_, False), (0, True)) -> False -- ·.
     ((x, _), (y, _)) | x == y -> True -- ·: or :·
 
--- | Returns the island state of one compare points
+-- | Returns the island state of a 1x2 grid
 initialState :: ComparePoints -> IslandState
 initialState (0, False) = None
 initialState (0, True) = New
 initialState (x, _) = Connected x
 
--- | Determines the state change when traversing from a compare point to the next
+-- | Determines the state change when traversing a 2x2 grid
 next :: IslandState -> ComparePoints -> ComparePoints -> IslandState
 -- Islandstate is preserved when traversing identical ComparePoints
 next state p1 p2 | p1 == p2 = state -- .. or :: or ··
@@ -92,6 +93,7 @@ next (Connected x) (y, True) (0, True) = Connected x -- :.
 next (Connected x) (0, True) (y, True) = Connected y -- .: This situation possibly merges 2 islands
 next (Connected x) (y, _) (z, _) = Connected x -- ·: or :·
 
+-- | Mark the boolean line with the current marker (True -> marker, False -> 0)
 mark :: Marker -> IslandState -> CompareLines -> Line Int
 mark marker New bs = markBy marker bs
 mark marker None bs = markBy 0 bs
@@ -105,5 +107,5 @@ indicator x True = x
 indicator _ False = 0
 
 increaseMarker :: IslandState -> Marker -> Marker
-increaseMarker New = (+) 1
-increaseMarker _ = id
+increaseMarker New marker = marker + 1
+increaseMarker _ marker = marker
