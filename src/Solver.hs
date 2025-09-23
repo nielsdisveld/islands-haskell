fd58107e187a7199161335665210d4912d45506c
Niels Disveld <nielsdisveld@gmail.com>
2025-09-24 00:12:04 +0200

    Documentation

--- src/Solver.hs
+++ src/Solver.hs
@@ -3,56 +3,76 @@ module Solver (solve) where
 import Distribution.Utils.Generic (fstOf3)
 import Line (Line, zeroes)
 
--- | Represents island count
+-- | Type alias for the final island count.
 type Count = Int
 
--- | Represents the current land marker
+-- | Marker used to identify different islands.
 type Marker = Int
 
--- | A tuple that represents the current point (Bool) compared to the marked previous point
-type ComparePoints = (Int, Bool)
+-- | Represents a pair of a marker from the previous line and a Boolean indicating
+-- current land presence.
+-- Used to track how land connects between rows.
+-- Values can be categorized by  : or . or   or · and these characters will be used
+-- to illustrate which situations are handled.
+type PairedCell = (Int, Bool)
 
--- | List of ComparePoint which can be seen as a 2-wide grid
-type CompareLines = Line ComparePoints
+-- | A line of `ComparePoints`, where each point pairs the current cell
+-- with the corresponding cell directly above it.
+--
+-- Think of it as:
+--
+-- >    Previous row:  [ 0, 0, 1, 1, 0 ]
+-- >    Current row:   [ 1, 0, 1, 1, 1 ]
+-- >    CompareLines:  [(0,1),(0,0),(1,1),(1,1),(0,1)]
+--
+-- Each (Int, Bool) = (marker from previous line, land in current line)
+type PairedLine = Line PairedCell
 
+-- | Describes the state of a land segment as it's being evaluated.
 data IslandState = None | New | Connected Int
   deriving (Eq, Show)
 
--- | Count how many islands on given input.
--- Assumed is that all line are of equal length
+-- | Count how many islands on given grid.
+-- Assumes all lines are of equal length
 solve :: [Line Bool] -> Count
 solve = fstOf3 . foldr go (0, 1, zeroes)
   where
     go :: Line Bool -> (Count, Marker, Line Int) -> (Count, Marker, Line Int)
     go ln (count, marker, prev) =
-      let twoLines = zip prev ln
-       in countNew count marker twoLines
+      let paired = zip prev ln
+       in processLine count marker paired
 
 -- | Compare line to previous line and count how many new islands are starting or merging.
--- Also return a transformed version of current line where the islands are labeled
-countNew :: Count -> Marker -> CompareLines -> (Count, Marker, Line Int)
-countNew count marker lns =
-  case splitConnected lns of
+-- Also return a transformed version of current line where the islands are marked
+processLine :: Count -> Marker -> PairedLine -> (Count, Marker, Line Int)
+processLine count marker paired =
+  case splitConnected paired of
     ([], []) -> (count, marker, [])
-    (land, rest) ->
-      let initial = initialState (head land)
-          (newCount, state) = exploreConnected count initial land
-          transformed = mark marker state land
+    (segment, rest) ->
+      let initial = initialState (head segment)
+          (newCount, state) = processConnected count initial segment
+          marked = mark marker state segment
           newMarker = increaseMarker state marker
-          (finalCount, finalMarker, finalTransformed) = countNew newCount newMarker rest
-       in (finalCount, finalMarker, transformed ++ finalTransformed)
+          (finalCount, finalMarker, restMarked) = processLine newCount newMarker rest
+       in (finalCount, finalMarker, marked ++ restMarked)
 
--- | Determine the state of a connected 2-wide grid
-exploreConnected :: Count -> IslandState -> CompareLines -> (Count, IslandState)
-exploreConnected count state (p1 : p2 : rest) =
+-- | Determine the state of a connected segment.
+processConnected :: Count -> IslandState -> PairedLine -> (Count, IslandState)
+processConnected count state (p1 : p2 : rest) =
   case (state, next state p1 p2) of
-    (Connected x, Connected y) | x /= y -> exploreConnected (count - 1) state (p2 : rest)
-    (_, nextState) -> exploreConnected count nextState (p2 : rest)
-exploreConnected count New _ = (count + 1, New) -- Found a new island (not connected to previous line)
-exploreConnected count state _ = (count, state)
+    (Connected x, Connected y) | x /= y -> processConnected (count - 1) state (p2 : rest)
+    (_, nextState) -> processConnected count nextState (p2 : rest)
+processConnected count New _ = (count + 1, New) -- Found a new island (not connected to previous line)
+processConnected count state _ = (count, state)
 
--- | Break two compare lines so that we split of a connected 2-wide grid from the rest
-splitConnected :: CompareLines -> (CompareLines, CompareLines)
+-- | Splits a `PairedLine` into a connected region and the rest.
+--
+-- Schematically:
+--
+-- >  :.. : .:··:
+-- >   ↑    ↑
+-- >  (x, rest)
+splitConnected :: PairedLine -> (PairedLine, PairedLine)
 splitConnected [] = ([], [])
 splitConnected [x] = ([x], [])
 splitConnected (x : y : xs) =
@@ -62,8 +82,8 @@ splitConnected (x : y : xs) =
        in (x : land, rest)
     else ([x], y : xs)
 
--- | Determines if a 2x2 grid is connected
-isConnected :: ComparePoints -> ComparePoints -> Bool
+-- | Determines if two adjacent cells in a line are connected as land
+isConnected :: PairedCell -> PairedCell -> Bool
 isConnected p1 p2 =
   case (p1, p2) of
     ((0, False), _) -> True -- By definition we called this connected
@@ -74,38 +94,38 @@ isConnected p1 p2 =
     ((_, False), (0, True)) -> False -- ·.
     ((x, _), (y, _)) | x == y -> True -- ·: or :·
 
--- | Returns the island state of a 1x2 grid
-initialState :: ComparePoints -> IslandState
+-- | Determines the initial state of a PairedCell
+initialState :: PairedCell -> IslandState
 initialState (0, False) = None
 initialState (0, True) = New
 initialState (x, _) = Connected x
 
--- | Determines the state change when traversing a 2x2 grid
-next :: IslandState -> ComparePoints -> ComparePoints -> IslandState
--- Islandstate is preserved when traversing identical ComparePoints
+-- | Determines the state change when traversing two PairedCells
+next :: IslandState -> PairedCell -> PairedCell -> IslandState
+-- Islandstate is preserved when traversing identical PairedCells
 next state p1 p2 | p1 == p2 = state -- .. or :: or ··
--- None
 next None (0, False) p = initialState p
--- New
 next New (0, True) (x, True) = Connected x -- .:
--- Connected
 next (Connected x) (y, True) (0, True) = Connected x -- :.
 next (Connected x) (0, True) (y, True) = Connected y -- .: This situation possibly merges 2 islands
 next (Connected x) (y, _) (z, _) = Connected x -- ·: or :·
 
 -- | Mark the boolean line with the current marker (True -> marker, False -> 0)
-mark :: Marker -> IslandState -> CompareLines -> Line Int
+mark :: Marker -> IslandState -> PairedLine -> Line Int
 mark marker New bs = markBy marker bs
 mark marker None bs = markBy 0 bs
 mark marker (Connected x) bs = markBy x bs
 
-markBy :: Marker -> CompareLines -> Line Int
+-- | Apply a specific marker to all land positions
+markBy :: Marker -> PairedLine -> Line Int
 markBy x = fmap (indicator x . snd)
 
+-- | Indicator function that marks when True
 indicator :: Marker -> Bool -> Int
 indicator x True = x
 indicator _ False = 0
 
+-- | Increase marker only if the current state introduced a new island
 increaseMarker :: IslandState -> Marker -> Marker
 increaseMarker New marker = marker + 1
 increaseMarker _ marker = marker
